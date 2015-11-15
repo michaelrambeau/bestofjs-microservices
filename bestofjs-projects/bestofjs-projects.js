c7ccc63bc038236da8af78bd0533e36a8212a64b
Michael Rambeau <mikeair@gmail.com>
2015-11-15 17:00:20 +0900

    Still debugging....

--- bestofjs-projects/bestofjs-projects.js
+++ bestofjs-projects/bestofjs-projects.js
@@ -248,7 +248,7 @@ function createSuperproject(project, report) {
 function populateProjects(allProjects, allTags) {
   var populate = function (project) {
     console.log('Populate project', project.name);
-    console.log('tag', project.tag[0].toString());
+    console.log('tag', project.tags[0].toString());
     var tags = allTags
       .filter( tag => project.tags.map( tag => tag.toString() ).indexOf(tag._id.toString()) > -1 )
       .map( tag => tag.code );
